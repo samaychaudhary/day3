2b019453e0120a2695510e7d8e1aa07ced96f7d5
“Samay <chaudharysamay98@gmail.com>
2022-07-05 17:56:32 +0545

    :hammer: added info

--- string.py
+++ string.py
@@ -19,3 +19,31 @@ print ("aryama"+" "+"Rushav")
 print ("Aryama"+str(10))
 print ("1020")
 print ("10"+"20")
+
+#print (10+int("As"))
+
+
+#variable in python
+
+a=11
+print (a)
+
+a=11
+print ("the first valu of a is",a)
+a="Apple"
+print ("the second value of a is",a)
+
+
+numericvalue = 11
+print ("the iehgwhwug",numericvalue)
+
+fruitname = "apple"
+print ("fruit name is",fruitname)
+
+#numeric_value ==>  snake_case_writing
+#numericValue ===> camelCaseWriting
+
+numeric_value = 11
+fruit_name = "Apple"
+
+print ("the jkrjg",numeric_value, fruit_name)
