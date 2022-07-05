9edd56ecdd8338389f23fa01703675dd686cbe9d
“Samay <chaudharysamay98@gmail.com>
2022-07-05 17:40:15 +0545

    :tada: begin with string concatenation

---
+++ string.py
@@ -0,0 +1,21 @@
+#print (r"this is /\/\/\ mountain")
+#print (r"this is \\\\ double back slash")
+
+print (2+3)
+print (9/3)
+print (2**4)
+print (5%2)
+
+#print (U+F972)   #replce + with 000
+print ("\U0001F972")
+
+print (round(2**3.14,3))
+
+#string concatenation
+
+print ("Aryama"+"Rushav")
+print ("aryama"+" "+"Rushav")
+#print ("Aryama"+10) cannot add because one is string anothet integer
+print ("Aryama"+str(10))
+print ("1020")
+print ("10"+"20")
