ee22cbc422b7f645267d4b6aa9a17d0e33ad34f5
“Samay <chaudharysamay98@gmail.com>
2022-07-06 17:13:03 +0545

    :hammer: ok

---
+++ day4.py
@@ -0,0 +1,6 @@
+first_name,second_name = input ("please provide your full name space separated").split(",")
+name_type = type (first_name)
+print ("your ful name is--->" + first_name+" "+second_name)
+print (name_type)
+print ("sam")
+
